51aa45f9f7a7d56aacd62d64f406697096cdd93e
Makzy-creator <134551327+Makzy-creator@users.noreply.github.com>
2023-09-13 19:42:45 +0100

    update

---
+++ src/Components/Header/FloatWriter.jsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+/**how long it will take for each phrase to enter and exit (in seconds) */
+const wait = 3;
+
+/** The list of phrases */
+const words = ["Web developer", "Graphic designer"];
+
+/**
+ * Renders a component that animates the display of phrases from an array.
+ *
+ * @param {Array<string>} words - An array of phrases to be displayed.
+ * @return {JSX.Element} The rendered component.
+ */
+
+export default function FloatWriter() {
+  /** used to store and change the current phrase that will come in from the [words] array */
+  const [currentIndex, setIndex] = useState(0);
+
+  // useEffect to start the animation when this component is loaded
+  useEffect(() => {
+    const int = setInterval(
+      () => {
+        /** the index of the next phrase to come in from the [words] array */
+        const nextIndex = currentIndex + 1;
+
+        // check if the next index is a valid index in the [words] array
+        if (nextIndex >= words.length) {
+          // if [nextIndex] is >= the length of [words] array, set index back to 0
+          // this will set the current phrase back to the first item in the [words] array
+          setIndex(0);
+        } else {
+          // add one to the [currentIndex]
+          // this will cause the next phrase to come in
+          setIndex(nextIndex);
+        }
+      },
+      wait * 1000 // thenumber of seconds to wait multiplied by 1000 to convert to milliseconds
+    );
+
+    return () => clearInterval(int);
+  }, [currentIndex]);
+
+  return (
+    <div className="relative">
+      <h1 className="relative invisible">And I'm a</h1>
+      <AnimatePresence>
+        {words.map((word, index) => {
+          return (
+            index == currentIndex && (
+              <h1
+                className="absolute flex top-0 [letter-spacing:2px] font-semibold text-2xl"
+                key={index}
+              >
+                {word.split("").map((char, i) => (
+                  <motion.span
+                    className="inline-block relative"
+                    // rules that apply before in view
+                    initial={{
+                      opacity: 0,
+                      translateY: -40,
+                    }}
+                    // rules that apply when in view
+                    animate={{
+                      opacity: 1,
+                      translateY: 0,
+                    }}
+                    // rules that apply on view exit
+                    exit={{
+                      opacity: 0,
+                      translateY: 15,
+                    }}
+                    // transition configuration
+                    transition={{
+                      delay: i * 0.15, // every character waits for (0.15 * its index) to come in and exit
+                      type: "spring", // for bouncy effect
+                      stiffness: 200,
+                      damping: 10,
+                    }}
+                    key={char + i}
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+              </h1>
+            )
+          );
+        })}
+      </AnimatePresence>
+    </div>
+  );
+}
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,65 @@
 import React from 'react'
-import CallToAction from './CallToAction'
+// import CallToAction from './CallToAction'
+import FloatWriter from './FloatWriter'
 import './Header.css'
+// import { useState } from "react";
 
 const Header = () => {
+
   return (
-    <header>
-      <div className="container hContainer">
-        <h5>Hello I'm</h5>
-        <h1>Favoured Maxwell</h1>
-        <h5 className='text-light'>Frontend Engineer</h5>
-        <CallToAction />
+    <header id='h-hero' className='h-hero flex'>
+      <div className="h-content">
+        <h1 className='text-4xl'>Hi! I'm Favour Maxwell</h1>
+        <div className='flex'>
+              <h1 className=' [letter-spacing:2px] font-semibold text-2xl'>And I'm </h1>
+              <FloatWriter/>
+        </div>
+        <p className='font-medium'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <span className=' text-white'>Fugiat, itaque placeat dicta accusamus illum maxime voluptas illo nesciunt deleniti
+          </span></p>
+        <div className='contact'>
+            <a href={<contact/>}>
+              Email: 
+              <span>makzymaxwell@gmail.com</span>
+            </a>
+        </div>
+        <div className='btn-buttons'>
+          <button 
+              type='download'
+              value={"Image"}
+              onClick={e => {
+                setDownloadCv(downloadCV)
+                setDownloadCv(e.target.value)
+              }}
+            > Download CV
+          </button>
+          <button 
+              type='email' 
+              onClick={() => <contact />}
+            > Hire Me
+            </button>
+        </div>
+        <div className='socialIcons'>
+              <ul>
+                <li>
+                    <a className="" aria-current="page" to="linkedin">
+                linkedin
+                    </a>
+                </li>
+                <li>
+                    <a className="" aria-current="page" to="Whatsapp">
+                Whatsapp
+              </a>
+                </li>
+                <li>
+                    <a className="" aria-current="page" to="twitter">
+                twitter
+              </a>
+                </li>
+              </ul>
+        </div>
+      </div>
+      <div>
+        <img src={'.././assets/img/boy.png'} alt='boy' />
       </div>
     </header>
   )
