c2b952f94d28b5b2b5921e96887938564468f60a
Makzy-creator <134551327+Makzy-creator@users.noreply.github.com>
2023-09-27 12:23:41 +0100

    changes made

---
+++ src/Components/Header/Blob.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Blob = () => {
+  return (
+    <div>
+        <svg
+            viewBox="0 0 500 500"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="100%"
+            id="blobSvg"
+            >
+            <path
+            fill="#12f7ff"
+            >
+            </path>
+        </svg>
+    </div>
+  )
+}
+
+export default Blob
+
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,68 +1,74 @@
-import React from 'react'
-// import CallToAction from './CallToAction'
-import FloatWriter from './FloatWriter'
-import './Header.css'
-// import { useState } from "react";
+import React from "react";
+import FloatWriter from "./FloatWriter";
+import "./Header.css";
+import boy from "../../assets/img/boy.png";
+import { FaLinkedinIn, FaWhatsapp, FaTwitter } from "react-icons/fa";
+import Blob from './Blob'
 
 const Header = () => {
-
   return (
-    <header id='h-hero' className='h-hero flex'>
+    <header id="h-hero" className="h-hero flex">
       <div className="h-content">
-        <h1 className='text-4xl'>Hi! I'm Favour Maxwell</h1>
-        <div className='flex'>
-              <h1 className=' [letter-spacing:2px] font-semibold text-2xl'>And I'm </h1>
-              <FloatWriter/>
+        <h1 className="text-4xl font-bold">Hi! I'm Favour Maxwell</h1>
+        <div className="flex">
+          <h1 className="inline-flex [letter-spacing:2px] font-semibold text-2xl">
+            And I'm{" "}
+          </h1>
+          <FloatWriter />
         </div>
-        <p className='font-medium'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <span className=' text-white'>Fugiat, itaque placeat dicta accusamus illum maxime voluptas illo nesciunt deleniti
-          </span></p>
-        <div className='contact'>
-            <a href={<contact/>}>
-              Email: 
-              <span>makzymaxwell@gmail.com</span>
-            </a>
+        <p className="font-medium">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          <span className=" text-white">
+            Fugiat, itaque placeat dicta accusamus illum maxime voluptas illo
+            nesciunt deleniti
+          </span>
+        </p>
+        <div className="contact font-medium">
+          <div className="email-info">
+            <h5 className="text-white font-bold">Email:</h5>
+            <span className="text-white">makzymaxwell@gmail.com</span>
+          </div>
         </div>
-        <div className='btn-buttons'>
-          <button 
-              type='download'
-              value={"Image"}
-              onClick={e => {
-                setDownloadCv(downloadCV)
-                setDownloadCv(e.target.value)
-              }}
-            > Download CV
+        <div className="btn-buttons">
+          <button
+            className="btn"
+            type="download"
+            value={"Image"}
+            onClick={(e) => {
+              setDownloadCv(downloadCV);
+              setDownloadCv(e.target.value);
+            }}
+          >
+            {" "}
+            Download CV
           </button>
-          <button 
-              type='email' 
-              onClick={() => <contact />}
-            > Hire Me
-            </button>
+          <button className="btn" type="email" onClick={() => <contact />}>
+            {" "}
+            Hire Me
+          </button>
         </div>
-        <div className='socialIcons'>
-              <ul>
-                <li>
-                    <a className="" aria-current="page" to="linkedin">
-                linkedin
-                    </a>
-                </li>
-                <li>
-                    <a className="" aria-current="page" to="Whatsapp">
-                Whatsapp
-              </a>
-                </li>
-                <li>
-                    <a className="" aria-current="page" to="twitter">
-                twitter
-              </a>
-                </li>
-              </ul>
+        <div className="socialIcons flex max-w-max mx-auto lg:mx-0">
+          <a href="#" aria-current="page">
+            <FaLinkedinIn />
+          </a>
+          <a href="#" aria-current="page">
+            <FaWhatsapp />
+          </a>
+          <a href="#" aria-current="page">
+            <FaTwitter />
+          </a>
         </div>
       </div>
-      <div>
-        <img src={'.././assets/img/boy.png'} alt='boy' />
+      <div className="relative">
+        <div className="">
+          <img className="boy-img" src={boy} alt="boy" />
+        </div>
+        <div className="liquid-shape">
+          <Blob />
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
